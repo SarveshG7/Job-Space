9c8480c6b2873c5931a14e932709553f7435e062
SarveshG7 <mastersarveshgore@gmail.com>
2025-02-04 02:18:11 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-space-39zi.onrender.com/', // this is for vite
+    origin:'https://job-space-39zi.onrender.com', // this is for vite
     credentials:true
 }
 app.use(cors(corsOptions));
